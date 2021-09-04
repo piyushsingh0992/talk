a5190e9f9d7aa8eab56536e02bb2f034a392e5d7
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-04 13:52:45 +0530

    counter-tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          piyush
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+       
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   const [counter, counterSetter] = useState(0);
+//   return (
+//     <>
+//       <div className="App">
+//         <h1 className="counter">{counter}</h1>
+//         <button
+//           onClick={() => {
+//             counterSetter((value) => value + 1);
+//           }}
+//           className="btn btn-primary increment"
+//         >
+//           Increase
+//         </button>
+//         <button
+//           onClick={() => {
+//             counterSetter((value) => value - 1);
+//           }}
+//           className="btn btn-secondary decrement"
+//         >
+//           Decrease
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,114 @@
-import { render, screen } from '@testing-library/react';
+
+
+import Enzyme,{ shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+//test to check we render as App component
+
+
+// test('render Application',()=>{
+//   const wrapper= shallow(<App/>);
+//   const app=wrapper.find(".App");
+//   expect(app.length).toBe(1)
+// })
+
+//test to check  we render counter
+
+
+
+//test to check we render increment button
+
+
+
+
+//test to check we render decrement button
+
+
+
+//*******************************************//
+
+//behaviour tests
+
+
+//test to check counter should start at zero
+
+
+
+
+//test to check Increasing the counter
+
+
+
+//test to check Decreasing the counter  the counter
+
+
+
+
+// solution
+
+
+
+
+// test('render Application',()=>{
+//   const wrapper= shallow(<App/>);
+//   const app=wrapper.find(".App");
+//   expect(app.length).toBe(1)
+// })
+
+
+// test('render counter',()=>{
+//   const wrapper= shallow(<App/>);
+//   const counter=wrapper.find(".counter");
+//   expect(counter.length).toBe(1)
+// })
+
+
+// test('render increment button ',()=>{
+//   const wrapper= shallow(<App/>);
+//   const increment=wrapper.find(".increment");
+//   expect(increment.length).toBe(1)
+// })
+
+
+// test('render decrement button ',()=>{
+//   const wrapper= shallow(<App/>);
+//   const decrement=wrapper.find(".decrement");
+//   expect(decrement.length).toBe(1)
+// })
+
+// //behaviour tests
+
+// test('counter should start at zero',()=>{
+//   const wrapper= shallow(<App/>);
+//   const counter=wrapper.find(".counter");
+//   expect(counter.text()).toBe("0")
+// })
+
+
+
+
+
+// test('Increasing the counter ',()=>{
+//   const wrapper= shallow(<App/>);
+//   const increment=wrapper.find(".increment");
+
+//   increment.simulate('click');
+
+//   const counter=wrapper.find(".counter");
+//   expect(counter.text()).toBe("1")
+// })
+
+
+// test('Decreasing the counter  the counter ',()=>{
+//   const wrapper= shallow(<App/>);
+//   const decrement=wrapper.find(".decrement");
+
+//   decrement.simulate('click');
+
+//   const counter=wrapper.find(".counter");
+//   expect(counter.text()).toBe("-1")
+// })
